10090886365c132bbdef8c9b20f34e6471b3a14f
KROEN V.O.F <admin@kroendesign.nl>
2015-12-30 19:23:45 +0100

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,7 @@
+<form action="?authact=logon" method="POST">
+
+
+<font color="black">Gebruikersnaam: </font><input type="text" size="20" name="user"><br>
+<font color="black">Wachtwoord: </font><input type="password" size="20" name="pass"><br>
+<button type="submit" >Inloggen</button></p>
+
